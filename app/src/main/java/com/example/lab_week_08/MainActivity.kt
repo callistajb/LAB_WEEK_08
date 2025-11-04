4da13a3c3f7f987e7fa3dd34275ac56eac1149bd
callistajb <callistabandono71@sma.belajar.id>
2025-11-04 22:36:14 +0700

    “Commit No. 2 – add first foreground service”

--- app/src/main/java/com/example/lab_week_08/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.lab_week_08
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.work.*
@@ -16,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     //Create an instance of a work manager
     //Work manager manages all your requests and workers
     //it also sets up the sequence for all your processes
-    private val workManager = WorkManager.getInstance(this)
+    private val workManager by lazy { WorkManager.getInstance(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
         }
 
         //Create a constraint of which your workers are bound to.
@@ -45,15 +57,12 @@ class MainActivity : AppCompatActivity() {
         //Create a one time work request that includes
         //all the constraints and inputs needed for the worker
         //This request is created for the FirstWorker class
-        val firstRequest = OneTimeWorkRequest
-            .Builder(FirstWorker::class.java)
+        val firstRequest = OneTimeWorkRequestBuilder<FirstWorker>()
             .setConstraints(networkConstraints)
             .setInputData(getIdInputData(FirstWorker.INPUT_DATA_ID, id))
             .build()
 
-        //This request is created for the SecondWorker class
-        val secondRequest = OneTimeWorkRequest
-            .Builder(SecondWorker::class.java)
+        val secondRequest = OneTimeWorkRequestBuilder<SecondWorker>()
             .setConstraints(networkConstraints)
             .setInputData(getIdInputData(SecondWorker.INPUT_DATA_ID, id))
             .build()
@@ -75,29 +84,48 @@ class MainActivity : AppCompatActivity() {
         //Here we're observing the returned LiveData and getting the
         //state result of the worker (Can be SUCCEEDED, FAILED, or CANCELLED)
         //isFinished is used to check if the state is either SUCCEEDED or FAILED
-        workManager.getWorkInfoByIdLiveData(firstRequest.id)
-            .observe(this) { info ->
-                if (info.state.isFinished) {
-                    showResult("First process is done")
-                }
+        workManager.getWorkInfoByIdLiveData(firstRequest.id).observe(this) { info ->
+            if (info != null && info.state.isFinished) {
+                showResult("First process is done")
             }
+        }
 
-        workManager.getWorkInfoByIdLiveData(secondRequest.id)
-            .observe(this) { info ->
-                if (info.state.isFinished) {
-                    showResult("Second process is done")
-                }
+        workManager.getWorkInfoByIdLiveData(secondRequest.id).observe(this) { info ->
+            if (info != null && info.state.isFinished) {
+                showResult("Second process is done")
+                launchNotificationService()
             }
+        }
     }
 
     //Build the data into the correct format before passing it to the worker as input
     private fun getIdInputData(idKey: String, idValue: String) =
-        Data.Builder()
-            .putString(idKey, idValue)
-            .build()
+        Data.Builder().putString(idKey, idValue).build()
 
     //Show the result as toast
     private fun showResult(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    //Launch the NotificationService
+    private fun launchNotificationService() {
+        //Observe if the service process is done or not
+        //If it is, show a toast with the channel ID in it
+        NotificationService.trackingCompletion.observe(this) { Id ->
+            showResult("Process for Notification Channel ID $Id is done!")
+        }
+
+        //Create an Intent to start the NotificationService
+        //An ID of "001" is also passed as the notification channel ID
+        val serviceIntent = Intent(this, NotificationService::class.java).apply {
+            putExtra(NotificationService.EXTRA_ID, "001")
+        }
+
+        //Start the foreground service through the Service Intent
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+
+    companion object {
+        const val EXTRA_ID = "Id"
     }
 }
